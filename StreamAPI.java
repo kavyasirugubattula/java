3876fbb73cbace7d574b56197600390f882712e3
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-09 10:36:13 +0530

    First commit

--- StreamAPI.java
+++ StreamAPI.java
@@ -7,9 +7,23 @@ public class StreamAPI {
      List<Integer> nums = Arrays.asList(3,7,9,6,2,5);
 
     //We can't reuse the Stream. we use onlu once and if we want print multiple times it gets error in runtime 
-     Stream<Integer> st1 = nums.stream();
+    //  Stream<Integer> st1 = nums.stream();
+    //  Stream<Integer> st2 = st1.filter(n -> n%2==0);
+    //  Stream<Integer> st3 = st1.map(n -> n*2);
 
-     st1.forEach(i->System.out.println(i));
+    //  st3.forEach(i->System.out.println(i));
+
+    // int result = nums.stream()
+    //                 .filter(n -> n%2==0)
+    //                 .map(n -> n*2)
+    //                 .reduce(0,(c,e) -> c+e);
+    // System.out.println(result);
+
+    Stream<Integer> sortedvalues = nums.stream()
+                                    .filter(n -> n%2 ==0)
+                                    .sorted();
+                                    
+    sortedvalues.forEach(i -> System.out.println(i));
 
     }
 }
